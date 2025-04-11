38b97b728410862facb91502070f9d84d77c0947
NdetecT306 <nstasya777@mail.ru>
2025-04-12 03:57:31 +1100

    Create task3.js

---
+++ Task3/task3.js
@@ -0,0 +1,20 @@
+function main() {
+    const input = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+    let numbers = parseInt(input[0]);
+    let count = 0;
+    let index = 1;
+    while (numbers > 0) {
+        const perem = parseInt(input[index++]);
+        let temp = perem;
+        while (temp != 0) {
+            const ost = temp % 10; 
+            if (ost % 3 === 0) {
+                count += ost; 
+            }
+            temp = Math.floor(temp / 10); 
+        }
+        numbers--;
+    }
+    console.log(count);
+}
+main();
